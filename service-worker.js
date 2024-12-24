1893968cf394665f0ed144a8eb88eba0e6ba437d
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-24 09:32:36 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,37 @@
-const CACHE_NAME = 'kasir-sepatu-cache-v1';
+const CACHE_NAME = 'kasir-sepatu-v1';
 const urlsToCache = [
     './',
     './index.html',
     './style.css',
     './app.js',
     './manifest.json',
-    './Android/media/com.whatsapp/WhatsApp/Media/WhatsApp ages/IMG-20241223-WA0003.jpg',
-    './Android/media/com.whatsapp/WhatsApp/Media/WhatsApp ages/IMG-20241223-WA0001.jpg',
-    './Android/media/com.whatsapp/WhatsApp/Media/WhatsApp ages/IMG-20241223-WA0002.jpg'
+    './img/sepatuA.jpg',
+    './img/sepatuB.jpg',
+    './img/sepatuC.jpg',
 ];
 
-// Install Service Worker
-self.addEventListener('install', (event) => {
+// Install
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Fetch Cached Resources
-self.addEventListener('fetch', (event) => {
+// Activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Activate and Cleanup Old Cache
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
