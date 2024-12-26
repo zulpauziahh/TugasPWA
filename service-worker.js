5f10dbe377ea4b1ab0dddd19518cd7e8fdd720c0
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-26 14:33:44 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,22 +15,34 @@ const urlsToCache = [
     'https://img.herstory.co.id/articles/archive_20220826/kopi-20220826-125544.jpg'
 ];
 
-// Install
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-// Activate
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
 self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then(keys => 
-            Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            )
-        )
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
