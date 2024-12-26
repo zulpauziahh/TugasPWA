ba8d80bdc722a781ad1e1b0ddb7bb933e26bccff
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-26 08:29:59 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,22 +8,17 @@ const urlsToCache = [
     './img/sepatuA.jpg',
     './img/sepatuB.jpg',
     './img/sepatuC.jpg',
-    './img/icon-192.png',
-    './img/icon-512.png'
 ];
 
-// Install Event
+// Install
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Caching resources...');
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Activate Event
+// Activate
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => 
@@ -32,14 +27,11 @@ self.addEventListener('activate', event => {
             )
         )
     );
-    console.log('Service Worker activated!');
 });
 
-// Fetch Event
+// Fetch
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
