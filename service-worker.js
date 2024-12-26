fee42cf019f581fabeec3828aedf7d7455678806
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-26 07:23:02 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,23 +2,29 @@ const CACHE_NAME = 'kasir-sepatu-v1';
 const urlsToCache = [
     './',
     './index.html',
+    './dashboard.html',
     './style.css',
     './app.js',
     './manifest.json',
     './img/sepatuA.jpg',
     './img/sepatuB.jpg',
     './img/sepatuC.jpg',
+    './img/icon-192.png',
+    './img/icon-512.png'
 ];
 
-// Install
+// Install Event
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Caching resources...');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-// Activate
+// Activate Event
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => 
@@ -27,11 +33,14 @@ self.addEventListener('activate', event => {
             )
         )
     );
+    console.log('Service Worker activated!');
 });
 
-// Fetch
+// Fetch Event
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
