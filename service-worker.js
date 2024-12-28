3301a913e36a7cdc3c1612941869127d071bbd51
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-28 17:42:02 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
     './manifest.json',
     'https://allofresh.id/blog/wp-content/uploads/2023/04/resep-es-timun-suri-1-1-1.jpg',
     'https://www.wandercooks.com/wp-content/uploads/2021/04/malaysian-pulled-tea-teh-tarik-2.jpg',
-    'https://travellingindonesia.com/wp-content/uploads/2022/06/Es-Pisang-Ijo-khas-Makassar-Dok.-Istimewa.jpg',
+    'https://id.images.search.yahoo.com/images/view;_ylt=AwrPrWto1W9noww56m3NQwx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2VmNWEyNGYwNmI2ZDA0NWU3NDM2YTkwZDRhNjczZTUyBGdwb3MDMjAEaXQDYmluZw--?back=https%3A%2F%2Fid.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Des%2Bpisang%2Bijo%2Bestetik%26ei%3DUTF-8%26type%3DE211ID1357G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D20&w=1140&h=550&imgurl=factsofindonesia.com%2Fwp-content%2Fuploads%2F2021%2F07%2Fes-pisang-ijo.jpg&rurl=https%3A%2F%2Ffactsofindonesia.com%2Findonesian-dessert-drink%2Fes-pisang-ijo-5&size=122KB&p=es+pisang+ijo+estetik&oid=ef5a24f06b6d045e7436a90d4a673e52&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=es-pisang-ijo+-+FactsofIndonesia.com&b=0&ni=140&no=20&ts=&tab=organic&sigr=OfEuJMwpwQil&sigb=2d7bkHdE2qdp&sigi=jK9LiG91susV&sigt=jMELkdRUke4M&.crumb=cXC05tSoCbe&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E211ID1357G0',
     'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/02/23/kopi-rempah-3400559857.jpg',
     'https://serikatnews.com/wp-content/uploads/2023/01/Bajigur.jpg',
     'https://assets.ladiestory.id/gallery/171246131220600903-ilustrasi.jpg',
