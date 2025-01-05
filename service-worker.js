4939e19cd141fe11a2746f83f655777399e76804
zulpauziahh <zlpapzhsenn@gmail.com>
2025-01-05 13:38:48 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
     './manifest.json',
     'https://allofresh.id/blog/wp-content/uploads/2023/04/resep-es-timun-suri-1-1-1.jpg',
     'https://www.wandercooks.com/wp-content/uploads/2021/04/malaysian-pulled-tea-teh-tarik-2.jpg',
-    'https://drive.google.com/file/d/11A6XPkMZ76o2p93ms_nHGSQ9LKj6Cu8I/view?usp=drivesdk.jpg', 
+    'https://id.images.search.yahoo.com/images/view;_ylt=AwrPrWu6IHpnd1o6SX3NQwx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzliZDVjMWVlYmM2ZjVkYWU2ODI2YzZiNTdlNTMzN2VhBGdwb3MDNjEEaXQDYmluZw--?back=https%3A%2F%2Fid.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Des%2Bselasih%2Bmelon%26type%3DE211ID1357G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26nost%3D1%26tab%3Dorganic%26ri%3D61&w=1200&h=630&imgurl=img-global.cpcdn.com%2Frecipes%2Fad94b2747140d464%2F1200x630cq70%2Fphoto.jpg&rurl=https%3A%2F%2Fcookpad.com%2Fid%2Fresep%2F983803-es-melon-selasih&size=77KB&p=es+selasih+melon&oid=9bd5c1eebc6f5dae6826c6b57e5337ea&fr2=piv-web&fr=mcafee&tt=Resep+Es+Melon+Selasih+oleh+hanhanny+-+Cookpad&b=61&ni=21&no=61&ts=&tab=organic&sigr=QiQdRmE86VG1&sigb=7Y4Y6a40an31&sigi=vMakXHI.HRgj&sigt=d7YdRMfL98O_&.crumb=cXC05tSoCbe&fr=mcafee&fr2=piv-web&type=E211ID1357G0.jpg', 
     'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/02/23/kopi-rempah-3400559857.jpg', 
     'https://serikatnews.com/wp-content/uploads/2023/01/Bajigur.jpg',
     'https://assets.ladiestory.id/gallery/171246131220600903-ilustrasi.jpg',
